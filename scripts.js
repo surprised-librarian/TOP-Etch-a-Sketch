2fd3d2f0a580680e6ade3292d698f90654ce6aa6
surprised-librarian <alex@esrl.org>
2025-01-08 14:41:32 -0500

    Added variable for grid squares. Added event listener for grid squares.

--- scripts.js
+++ scripts.js
@@ -8,6 +8,16 @@ function createGrid(size) {
         let side = 100 / size;
         gridItem.style.width = side + '%';
         gridItem.style.height = side + '%';
+        gridItem.style.backgroundColor = 'blue';
         gridContainer.appendChild(gridItem);
     }
 }
+
+createGrid(16);
+
+let gridItems = document.querySelectorAll('.grid-item');
+
+gridItems.addEventListener('mouseover', function() {
+    gridItems.style.backgroundColor = 'black';
+}
+
