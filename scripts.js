771946a58374c908831d0d598bc44124629740cb
surprised-librarian <alex@esrl.org>
2025-01-08 17:12:57 -0500

    Created grid layout for the website

--- scripts.js
+++ scripts.js
@@ -1,23 +1,30 @@
 let gridContainer = document.querySelector('#grid-container');
+gridContainer.style.width = window.innerHeight - 100 + 'px';
+gridContainer.style.height = window.innerHeight - 100 + 'px';
+
+function addEvent(arg) {
+    console.log('arg');
+    document.querySelector(arg).style.backgroundColor = '#00A36C';
+}
+
 
 function createGrid(size) {
     let gridSize = size * size;
+    //let screeenSize = screen.width;
     for (let i = 0; i < gridSize; i++) {
         let gridItem = document.createElement('div');
         gridItem.classList.add('grid-item');
-        let side = 100 / size;
-        gridItem.style.width = side + '%';
-        gridItem.style.height = side + '%';
-        gridItem.style.backgroundColor = 'blue';
+        gridItem.id = 'grid-item-' + (i + 1);
+        gridItem.style.flex = 100 / size + '%';
+        gridItem.setAttribute('onmouseover', 'addEvent(' + gridItem.id + ')');
         gridContainer.appendChild(gridItem);
     }
+    return size;
 }
 
-createGrid(16);
+createGrid(4);
 
-let gridItems = document.querySelectorAll('.grid-item');
 
-gridItems.addEventListener('mouseover', function() {
-    gridItems.style.backgroundColor = 'black';
-}
+    
+
 
